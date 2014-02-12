84db915ad6ba96e25b878f6cbe7964c345c7738d
nthmost <nthmost@users.noreply.github.com>
2014-02-11 19:51:22 -0800

    stubs of decorator functions to write

---
+++ secureconfig/decorators.py
@@ -0,0 +1,13 @@
+
+# stubs of ideas for decorators
+
+def encrypted(func):
+    '''make sure data transmissions are encrypted'''
+    return func
+
+
+def protected(func):
+    '''makes sure files are encrypted when written, and uses decrypt when read.'''
+    return func
+
+
