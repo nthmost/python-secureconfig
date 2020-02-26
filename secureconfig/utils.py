3c669040c6d314f65501e46a40c51452c59bb9cb
andy <andy@shady.org>
2020-02-26 17:22:51 +0000

    fixed for PEP8

--- secureconfig/utils.py
+++ secureconfig/utils.py
@@ -5,29 +5,31 @@ from random import sample, choice
 
 ACCEPTED_SYMBOLS = '_-)(&^#@!.'
 
+
 def safe_pwgen(length=32, symbols=ACCEPTED_SYMBOLS):
-    '''returns a non-human-readable password of length=length (default 32). 
-        
+    """returns a non-human-readable password of length=length (default 32).
+
         :param length: (int) desired length of password (default: 32)
         :param symbols: non-alphanumeric symbols to accept as part of the password.
                 default ACCEPTED_SYMBOLS=  _-)(&^#@!.
-        '''
+        """
     chars = string.letters + string.digits + symbols
     return ''.join(choice(chars) for _ in range(length))
 
+
 def encrypt_file(ck_obj, infile, outfile):
     enctxt = ck_obj.encrypt(open(infile, 'r').read())
-    f=open(outfile, 'wb')
+    f = open(outfile, 'wb')
     f.write(enctxt)
     f.close()
+
 
 def decrypt_file(ck_obj, infile, outfile=''):
     txt = ck_obj.decrypt(open(infile, 'rb').read())
     if outfile:
-        f=open(outfile, 'w')
+        f = open(outfile, 'w')
         f.write(txt)
-        f.close()     
+        f.close()
         return outfile
     else:
         return txt
-
